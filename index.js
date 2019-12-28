1505e29697eda4a80a94953a956367b7b4670975
Mohammed Noori <kmanoorib@gmail.com>
2019-12-28 04:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,6 +81,14 @@ const indigo = indg;
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
+// const muppet = {
+//   name: 'Kermit',
+//   color: 'green',
+//   song: 'The Rainbow Connection',
+//   job: 'Host of The Muppet Show',
+//   partner: 'Miss Piggy'
+// };
+
 
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
